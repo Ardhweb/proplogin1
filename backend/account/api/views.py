c66d516b3e0c684b7bdc41f2cb330bcb9bfee334
Ardhweb <sun.projectsdev@gmail.com>
2025-07-28 11:26:10 +0530

    api register new user

--- backend/account/api/views.py
+++ backend/account/api/views.py
@@ -2,6 +2,11 @@ from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework import viewsets
 from account.api.serializers import UserSerializer ,UserRegisterSerializer
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 class UserViewSet(viewsets.ModelViewSet):
     http_method_name = ('get', 'patch')
@@ -24,7 +29,30 @@ class UserViewSet(viewsets.ModelViewSet):
         self.check_object_permissions(self.request, obj)
         return obj
 
-class UserRegisterViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny)
+class UserRegisterViewSet(viewsets.ViewSet):
+    '''
+    authentication: Bearer Token
+    '''
+    http_method_names = ('post')
+    permission_classes = (AllowAny,)
+    authentication_classes = [] # disables JWT or any auth cause for authentication user have to exdists in our system
     serializer_class = UserRegisterSerializer
-    http_method_names = ['post']
+    
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        #self.perform_create(serializer)
+        obj = serializer.save()
+        refresh = RefreshToken.for_user(obj)
+        res = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+        #headers =self.get_success_headers(serializer.data)
+        #return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({
+            "user":serializer.data,
+            "refresh":res["refresh"],
+            "token":res["access"]
+        },status=status.HTTP_201_CREATED)
--- backend/backend/routers.py
+++ backend/backend/routers.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import SimpleRouter
-from  account.api.views import UserViewSet
+from  account.api.views import UserViewSet,UserRegisterViewSet
 
 '''centerlized routing process to  create endpoint'''
 router = SimpleRouter()
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', UserRegisterViewSet,basename='auth-register')
 ''' added each new app , api routes viewset to  include as routing endpoint '''
 
 urlpatterns = router.urls
